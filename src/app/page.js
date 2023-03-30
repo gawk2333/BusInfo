e1228bcb005ca6f46a02e04d9bac9b25cf386b88
Kang Gao <kang.g1988@gmail.com>
2023-03-31 01:16:21 +1300

    fixed no html link for pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,20 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className={styles.grid}>
-      <a href="/map" className={styles.card}>
+      <Link href="/map" className={styles.card}>
         <h2 className={inter.className}>
           Map <span>-&gt;</span>
         </h2>
         <p className={inter.className}>
           Find the bus stop number from the map.
         </p>
-      </a>
+      </Link>
     </div>
   );
 }
