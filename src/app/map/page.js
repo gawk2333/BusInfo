e90bf614160d8d49dff2cb90d9f1973a9d5f289d
Kang Gao <kang.g1988@gmail.com>
2023-03-31 19:03:32 +1300

    Move map out to components folder and set map route as server

--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,26 +1,8 @@
-"use client";
-import styles from "./page.module.css";
-import Stops from "@/components/Stops";
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
+import dynamic from "next/dynamic";
 
-export default function Map() {
-  return (
-    <MapContainer
-      center={[-43.5321, 172.6362]}
-      zoom={14}
-      maxZoom={18}
-      minZoom={10}
-      className={styles.map}
-      placeholder={<div>this is a placeholder</div>}
-    >
-      <Stops />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
-  );
+export default function MapPage() {
+  const Map = dynamic(() => import("@/components/Map"), {
+    ssr: false,
+  });
+  return <Map />;
 }
