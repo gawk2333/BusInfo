43659a3b117533a7a3a750d924a7bba82721ff8d
Kang Gao <kang.g1988@gmail.com>
2023-04-06 12:09:52 +1200

    add node-fetch

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import { initDefaultConnection } from "@/lib/mongodb/mongodb";
+import fetch from "node-fetch";
 
 const domainUrl =
   process.env.NODE_ENV === "production"
